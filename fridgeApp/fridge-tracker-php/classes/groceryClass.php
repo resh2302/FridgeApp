6b8410af6ae091b57d9d458a8dbacb40ab6c4b0f
patelshalini <patelshallu21@yahoo.com>
2014-06-16 15:44:40 -0400

    grocery-Fridge-Shalini

--- fridgeApp/fridge-tracker-php/classes/groceryClass.php
+++ fridgeApp/fridge-tracker-php/classes/groceryClass.php
@@ -12,7 +12,29 @@ class groceryClass{
                 $stm->execute();
                 $row = $stm->fetch(); //PDO::FETCH_ASSOC
                 return $row;
-        }  
+        }
+        public function InsertFridge($ID,$ItemName,$FridgeID){
+            $db=  FridgeDB::getDB();
+            $q="Insert into GroceryFridge where ID=:ID,ItemName=:ItemName,FridgeID=:FridgeID";
+            $stm=$db->prepare($q);
+            $stm->bindParam(':ID',$ID);
+            $stm->bindParam(':ItemName',$ItemName);
+            $stm->bindParam(':FridgeID',$FridgeID);
+            $stm->execute();
+            $row=$stm->fetch();
+            return $row;
+        }
+        public function InsertFreezer($ID,$ItemName,$Freezer_id){
+           $db=  FridgeDB::getDB();
+            $q="Insert into GroceryFreezer where ID=:ID,ItemName=:ItemName,Freezer_id=:Freezer_id";
+            $stm=$db->prepare($q);
+            $stm->bindParam(':ID',$ID);
+            $stm->bindParam(':ItemName',$ItemName);
+            $stm->bindParam(':Freezer_id',$Freezer_id);
+            $stm->execute();
+            $row=$stm->fetch();
+            return $row;  
+        }
 }
 
 
--- fridgeApp/fridge-tracker-php/groceryList.php
+++ fridgeApp/fridge-tracker-php/groceryList.php
@@ -1,6 +1,12 @@
 <?php
 require_once 'checkSession.php';
-require_once 'groceryClass.php';
+ $refID = $_SESSION['refID'];
+ 
+require_once './classes/groceryClass.php';
+    $gc = new groceryClass();
+    $GroceryFridge = $gc->getGrocerybyID();
+    
+require_once './classes/FridgeDB.php';   
 require_once './classes/Refrigerator.php';
 require_once './classes/FridgeFood.php';
 require_once './classes/FreezerFood.php';
@@ -8,13 +14,60 @@ require_once 'FirePHPCore-0.3.2/lib/FirePHPCore/fb.php';
 
 
 ?>
-<!DOCTYPE html>
-<html lang="en">
+<html>
 <head>
-<meta charset="utf-8" />
-<meta http-equiv="x-ua-compatible" content="ie=edge" />
-<meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1, maximum-scale=1" />
-<title>Fridge</title>
+	<title>Grocery list</title>
+	<link rel="stylesheet" href="./assets/stylesheets/demo.css" />
+	  <link rel="stylesheet" href="./assets/stylesheets/unsemantic-grid-responsive.css" />
+	<link rel="stylesheet" type="text/css" href="css/main.css">
+</head>
 <body>
-    
+	<header>
+      <div id="gl_frd_header">
+        <div class="back">
+          <img src="images/white_back.png" alt="back">
+        </div>
+      <h2>Fridge</h2>
+      </div>
+    </header>
+    <div id="gl_frd_main">
+    	<h3>Refridgerator</h3>
+    <div class="radio">
+ <form method="POST" action="groceryList.php" >
+            <h3> Add food items </h3>
+           <input id="frd_list" type="radio" name="frd_list" value="frd_list">
+			<label for="frd_list"></label>
+			<input type="text" placeholder="work" />
+			<br/>
+               
+                <?php
+                    foreach ($InsertFridge as $groceryfridge)
+                    {
+                       
+                    }
+                ?>
+            <input type="submit" value="Done" />
+        
+        
+        </form>
+		
+			
+		
+			<!-- <input id="female" type="radio" name="gender" value="female">
+			<label for="female"></label>
+			<input id="female" type="radio" name="gender" value="female">
+			<input id="male" type="radio" name="gender" value="male">
+			<label for="female"></label> -->
+			
+		</div>
+
+	</div>
+
+	
+	
+
+
 </body>
+</html>
+
+   
