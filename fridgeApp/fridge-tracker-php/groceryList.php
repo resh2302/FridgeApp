8afe1138a615119ba2c0cdf77e0c284a2435cdaf
patelshalini <patelshallu21@yahoo.com>
2014-06-16 09:22:17 -0400

    working

--- fridgeApp/fridge-tracker-php/groceryList.php
+++ fridgeApp/fridge-tracker-php/groceryList.php
@@ -1,8 +1,20 @@
 <?php
+require_once 'checkSession.php';
+require_once 'groceryClass.php';
+require_once './classes/Refrigerator.php';
+require_once './classes/FridgeFood.php';
+require_once './classes/FreezerFood.php';
+require_once 'FirePHPCore-0.3.2/lib/FirePHPCore/fb.php';
 
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="utf-8" />
+<meta http-equiv="x-ua-compatible" content="ie=edge" />
+<meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1, maximum-scale=1" />
+<title>Fridge</title>
+<body>
+    
+</body>
