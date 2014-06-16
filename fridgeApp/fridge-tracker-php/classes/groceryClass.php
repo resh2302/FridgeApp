edb6061a511a28629aaa6f74641f17a00ee58730
patelshalini <patelshallu21@yahoo.com>
2014-06-16 09:23:40 -0400

    groceryclass

--- fridgeApp/fridge-tracker-php/classes/groceryClass.php
+++ fridgeApp/fridge-tracker-php/classes/groceryClass.php
@@ -1,8 +1,18 @@
 <?php
+require_once 'FridgeDB.php';
+require_once 'FirePHPCore-0.3.2/lib/FirePHPCore/fb.php';
 
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+class groceryClass{
+     public function getGrocerybyID($id)
+        {
+                $db = FridgeDB::getDB();
+                $q = "SELECT * FROM GroceryFridge WHERE ID = :id";
+                $stm = $db->prepare($q);
+                $stm->bindParam(':id', $id);
+                $stm->execute();
+                $row = $stm->fetch(); //PDO::FETCH_ASSOC
+                return $row;
+        }  
+}
+
 
