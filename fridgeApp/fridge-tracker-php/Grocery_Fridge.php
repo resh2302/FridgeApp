3a6a244db97d068ed4cf1e99b18cd45acac777cc
patelshalini <patelshallu21@yahoo.com>
2014-06-16 22:56:06 -0400

    groceryfridge

---
+++ fridgeApp/fridge-tracker-php/Grocery_Fridge.php
@@ -0,0 +1,17 @@
+<?php
+?>
+<html>
+<head>
+	<title>Grocery list</title>
+	<link rel="stylesheet" type="text/css" href="css/main.css">
+</head>
+<body id="gl_body">
+	<div id="gl_splash_wrapper">
+	<div class="rd_btn" id="gl_frd" a href="grocerylist.php">Fridge</div>
+	<div class="rd_btn"  id="gl_frz" a href="groceryFreezer.php">Freezer</div>
+	</div>
+
+
+</body>
+</html>
+
